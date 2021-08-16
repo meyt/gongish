0e3c47c13a8f6c3afeef0e9823668e684da3f550
meyt <pasd3000@gmail.com>
2021-08-16 17:06:12 +0430

    Fix configuration context

--- gongish/configuration.py
+++ gongish/configuration.py
@@ -1,12 +1,13 @@
 import os
 
 from pymlconf import Root
-from gongish import __version__
 
 
 class ConfigurationMixin:
     def __init__(self):
-        context = {"version": __version__}
+        from gongish import __version__
+
+        context = {"gongish_version": __version__}
         context.update(os.environ)
         data = {"debug": True}
         self.config = Root(data=data, context=context)
