ce3bed0a6de9020d91d6de143d3810da004b902d
meyt <pasd3000@gmail.com>
2025-05-18 05:57:29 +0330

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 def read_version(module_name):
-    from re import match, S
-    from os.path import join, dirname
+    from os.path import dirname, join
+    from re import S, match
 
     with open(join(dirname(__file__), module_name, "__init__.py")) as f:
         return match(r".*__version__.*('|\")(.*?)('|\")", f.read(), S).group(2)
@@ -21,7 +21,8 @@ setup(
     keywords="web tool-chain",
     packages=find_packages(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
@@ -30,7 +31,15 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
     entry_points={"console_scripts": ["gongish = gongish.cli:main"]},
     install_requires=["pymlconf == 2.2.0"],
